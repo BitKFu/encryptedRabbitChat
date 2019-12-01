13bde2a83954b48157499071460305717dea188e
Gerhard Stephan <gerhard.stephan@timepunch.de>
2019-12-01 15:10:14 +0100

    First Implementation of the encrypted rabbit MQ group chat

--- src/EncryptedRabbitChat/ERC.netFramework/Properties/AssemblyInfo.cs
+++ src/EncryptedRabbitChat/ERC.netFramework/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ERC.netFramework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("ERC.netFramework")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyCompany("TimePunch KG")]
+[assembly: AssemblyProduct("Encrypted Rabbit MQ Chat")]
+[assembly: AssemblyCopyright("Copyright © TimePunch KG 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
