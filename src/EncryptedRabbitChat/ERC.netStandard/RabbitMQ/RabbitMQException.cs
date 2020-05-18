912fe5426fe3e6c0958ee8e23d2c2be9d7f8356b
bitkfu <gerhard.stephan@timepunch.de>
2020-05-18 11:57:50 +0200

    Included the RabbitMQ/RabbitMQException.cs

---
+++ src/EncryptedRabbitChat/ERC.netStandard/RabbitMQ/RabbitMQException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERC.RabbitMQ
+{
+    public class RabbitMQException : Exception
+    {
+        public enum ExceptionType
+        {
+            /// <summary>Thrown if the client queue is not available</summary>
+            NoClientQueue,
+
+            /// <summary>Thrown if the server queue is not available</summary>
+            NoServerQueue,
+
+            /// <summary>Thrown if the client does not receive server handshake data</summary>
+            NoServerHandshakeData,
+        }
+
+        public RabbitMQException(ExceptionType exceptionType)
+        {
+            Type = exceptionType;
+        }
+
+        public ExceptionType Type { get; }
+    }
+}
